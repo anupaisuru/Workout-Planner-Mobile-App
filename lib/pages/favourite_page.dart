91330270a11828be743a512fa83ebce59b33a701
anupaisuru <anupaisuru2002@gmail.com>
2025-10-24 19:03:53 +0530

    done favorite page

--- lib/pages/favourite_page.dart
+++ lib/pages/favourite_page.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:workout_planner/constants/colors.dart';
+import 'package:workout_planner/constants/responsive.dart';
+import 'package:workout_planner/data/user_data.dart';
+import 'package:workout_planner/models/equipment_model.dart';
+import 'package:workout_planner/models/exercise_model.dart';
 
 class FavouritePage extends StatefulWidget {
   const FavouritePage({super.key});
@@ -8,12 +14,169 @@ class FavouritePage extends StatefulWidget {
 }
 
 class _FavouritePageState extends State<FavouritePage> {
+  //date and time fomatters
+  final DateFormat formatter = DateFormat("EEEE,MMMM");
+  final DateFormat dayFormat = DateFormat("dd");
+
+  //user data
+  final userData = user;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Center(child: Text("favourite page"),),
+    DateTime now = DateTime.now();
+    String formattedDate = formatter.format(now);
+    String formatterDay = dayFormat.format(now);
+
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(kDefaultPadding),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "$formattedDate $formatterDay",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w500,
+                    color: kSubtitleColor,
+                  ),
+                ),
+                Text(
+                  userData.fullName,
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w700,
+                    color: kMainColor,
+                  ),
+                ),
+                SizedBox(height: 15),
+                Text(
+                  "Here are all your favorited workouts",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w600,
+                    color: kGradientBottomColor,
+                  ),
+                ),
+                SizedBox(height: 20),
+                //exercise grid view
+                GridView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: kDefaultPadding,
+                    mainAxisSpacing: kDefaultPadding,
+                    childAspectRatio: 16 / 17,
+                  ),
+                  itemCount: userData.favExerciseList.length,
+                  itemBuilder: (context, index) {
+                    Exercise favExercise = userData.favExerciseList[index];
+                    return Card(
+                      color: Colors.grey.withValues(alpha: 0.2),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8),
+                        child: Column(
+                          children: [
+                            Text(
+                              favExercise.exerciseName,
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            Image.asset(
+                              favExercise.exerciseImgUrl,
+                              width: 100,
+                              fit: BoxFit.cover,
+                            ),
+                            Text(
+                              "${favExercise.noOfMinutus.toString()} min workout",
+                              style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                                color: kMainPinkColor,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
+
+                SizedBox(height: 15),
+                Text(
+                  "Here are all your favorited Equipments",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w600,
+                    color: kGradientBottomColor,
+                  ),
+                ),
+                SizedBox(height: 20),
+                //exercise grid view
+                GridView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: kDefaultPadding,
+                    mainAxisSpacing: kDefaultPadding,
+                    childAspectRatio: 16 / 25,
+                  ),
+                  itemCount: userData.favEquipmentList.length,
+                  itemBuilder: (context, index) {
+                    Equipment favEquipment = userData.favEquipmentList[index];
+                    return Card(
+                      color: Colors.grey.withValues(alpha: 0.2),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8),
+                        child: Column(
+                          children: [
+                            Text(
+                              favEquipment.equipmentName,
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            Image.asset(
+                              favEquipment.equipmentImageUrl,
+                              width: 100,
+                              fit: BoxFit.cover,
+                            ),
+                            Text(
+                              "${favEquipment.noOfMinutus.toString()} min workout",
+                              style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                                color: kMainPinkColor,
+                              ),
+                            ),
+                            SizedBox(height: 12),
+                            Text(
+                              favEquipment.equipmentDescription,
+                              style: TextStyle(
+                                fontSize: 13,
+                                fontWeight: FontWeight.normal,
+                                color: kMainColor,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
